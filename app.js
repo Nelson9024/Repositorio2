549d4e186ee2a91137079534c32bfed4f2b5ef80
Nelson9024 <naguilardavid@gmail.com>
2025-04-11 10:53:52 -0500

    mi primer commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const showMessage =(message) => {
+    return message
+}
+console.log(showMessage("Hola cabeza de bola")) 
+console.log("Hola marx cruz")
+var a = 7
+var b = 10
+ 
+var r= a + b;
+console.log(`el resultado es:${r}`)
+console.log("Ya lo enlace ")
